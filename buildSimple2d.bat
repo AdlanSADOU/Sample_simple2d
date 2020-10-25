f77be511160f70ead8d9543c0f312935fc10dbab
Adlan Sadou <adlan.sadou@epitech.eu>
2020-10-25 09:32:16 +0100

    minor changes

---
+++ buildSimple2d.bat
@@ -0,0 +1,3 @@
+cd deps/simple2d
+nmake /f NMakefile all install
+cd ../../
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,41 @@
 #include "simple2d.h"
 
+// SDL_main.h is included automatically from SDL.h
+#ifdef main
+#undef main
+#endif
+
+struct sfSprite
+{
+    S2D_Sprite *S2D_sprite = NULL;
+    S2D_FRect rect;
+    S2D_Vec2f position;
+};
+
 S2D_Window *window = NULL;
 S2D_Sprite *sprite = NULL;
-float speed = 0.4f;
+float speed = 2.f;
+
+void contains_point_rect(S2D_FRect rect, S2D_Vec2f vec)
+{
+    
+}
 
 void move_sprite(SDL_Keycode key)
 {
     switch (key)
     {
     case SDLK_w:
-        (sprite->y) -= 1 * speed * window->loop_ms;
+        (sprite->y) -= 1 * speed;
         break;
     case SDLK_a:
-        (sprite->x) -= 1 * speed * window->loop_ms;
+        (sprite->x) -= 1 * speed;
         break;
     case SDLK_s:
-        (sprite->y) += 1 * speed * window->loop_ms;
+        (sprite->y) += 1 * speed;
     break;
     case SDLK_d:
-        (sprite->x) += 1 * speed * window->loop_ms;
+        (sprite->x) += 1 * speed;
         break;
     default:
         break;
@@ -45,7 +62,7 @@ void on_key(S2D_Event e)
 
 void update()
 {
-    
+    printf("mouse (%d, %d)\n", window->mouse.x, window->mouse.y);
 }
 
 void render()
@@ -56,17 +73,15 @@ void render()
 int main(int argc, char const *argv[])
 {
     window = S2D_CreateWindow("Awesome Sample", 800, 600, update, render, S2D_RESIZABLE);
+    window->vsync = false;
     window->on_key = on_key;
-    sprite = S2D_CreateSprite("assets/profile.png");
 
+    sprite = S2D_CreateSprite("assets/profile.png");
     if (sprite) {
         sprite->width = 128;
         sprite->height = 128;
     } else printf("could not load sprite\n");
 
-    S2D_GL_PrintContextInfo(window);
-    printf("Hello Simple2D!\n");
     S2D_Show(window);
-    system("dir");
     return 0;
 }
